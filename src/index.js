def54ca0ca9fe18b98bd6165f345ff9cb577ff0f
Anant Patel <anantpatel07012003@gmail.com>
2022-12-30 12:12:54 +0000

    Update 9 files

--- src/index.js
+++ src/index.js
@@ -1,13 +1,7 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-
-import App from "./App";
-
-const rootElement = document.getElementById("root");
-const root = createRoot(rootElement);
-
-root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+import React from "react";
+import ReactDOM from "react-dom";
+import "../src/index.css";
+import Keeper from "./Keeper.jsx"
+ReactDOM.render(
+    <Keeper />
+,document.getElementById("root"));
